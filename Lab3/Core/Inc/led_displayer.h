aaf5309b11367bbff2fa690f29a82e4093470744
Ngo Duc Luan <luan.ngoduc@hcmut.edu.vn>
2024-11-11 23:38:18 +0700

    add led_displayer

--- Lab3/Core/Inc/led_displayer.h
+++ Lab3/Core/Inc/led_displayer.h
@@ -8,6 +8,13 @@
 #ifndef INC_LED_DISPLAYER_H_
 #define INC_LED_DISPLAYER_H_
 
+#include "global.h"
 
+void display_SEG_A(int counter);
+void display_SEG_B(int counter);
+void display_SEG_C(int counter);
+void display_SEG_D(int counter);
+void update7SEG_AB(int data);
+void update7SEG_CD(int data);
 
 #endif /* INC_LED_DISPLAYER_H_ */
